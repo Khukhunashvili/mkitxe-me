c5b7447813ca2a46453d013c5bd259a0e0d81a01
FiNkacho <sabaxuxunashvili@gmail.com>
2018-08-31 12:13:17 +0400

    Implemented Index page

--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    # if User is authenticated redirect to dashboard.
+	if request.user.is_authenticated:
+        pass
+	return render(request, 'main/index.html')
