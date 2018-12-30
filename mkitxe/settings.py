4cdd409dad44dc47be0fd07bfc5d9b658819b153
FiNkacho <sabaxuxunashvili@gmail.com>
2018-12-30 22:43:11 +0400

    getting secret key from environment variables

--- mkitxe/settings.py
+++ mkitxe/settings.py
@@ -11,11 +11,12 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-from .secret_keys import SECRET_KEY
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SECRET_KEY = config('SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
